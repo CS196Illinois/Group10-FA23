f7a2a3b7993786bff4e9905cd4ecfe42a748e4fd
Ila Petrovic <ilapetrovic@wirelessprv-10-194-165-18.near.illinois.edu>
2023-09-30 15:04:08 -0500

    update

--- Research/ilacp2/research.py
+++ Research/ilacp2/research.py
@@ -4,32 +4,39 @@ from dateutil import parser
 from haversine import *
 
 
-
+#takes file as input
 def motionFunc( file ):
     parsed_file = gpxpy.parse(file)
+    #we want total distance and time to find avg velocity
+    totalDistance = 0
+    totalDuration = 0
 
+    #going over each track
     for track in parsed_file.tracks:
+        #going over each segment
         for segment in parsed_file.segments:
+            #set previousPoint to be uninitialzed at first
             previousPoint = ""
-            totalDistance = 0
-            totalTime = 0
 
             for currentPoint in segment.points:
                 if previousPoint == "":
                     previousPoint = currentPoint
                 
+                #not sure about this way of getting distance
                 dist = mpu.haversine((previousPoint.latitude, previousPoint.longitude), (currentPoint.latitude, currentPoint.longitude))
                 totalDistance += dist
-                time = (previousPoint.time - currentPoint.time).total_seconds()
-                totalTime += time
-
-                if time == 0:
-                    previousPoint = currentPoint
-                    continue
-
+                duration = (previousPoint.time - currentPoint.time).total_seconds()
+                totalDuration += duration
+  
+                #update previousPoint
                 previousPoint = currentPoint
 
-    avgVelocity = totalDistance/totalTime
+    avgVelocity = totalDistance/totalDuration
+    return avgVelocity
+
+git config --global user.email "ilacpetrovic@gmail.com"
+git config --global user.name "Ila Petrovic"
+
 
 
 
